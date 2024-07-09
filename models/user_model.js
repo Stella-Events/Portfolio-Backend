c67ab6f71d8987dd90973c230dae464920d802a3
encrentsil <nyamekye.nesther.esther599@gmail.com>
2024-07-09 11:08:44 +0000

    User Model Created

---
+++ models/user_model.js
@@ -0,0 +1,106 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  user: {
+    firstName: { type: String },
+    lastName: { type: String },
+    otherNames: { type: String },
+    username: { type: String, unique: true },
+    email: { type: String, unique: true },
+    termsAndConditions: { type: Boolean },
+    password: { type: String },
+  },
+
+  userProfile: {
+    profilePicture: { type: String },
+    sex: { type: String, enum: ["male", "female"] },
+    maritalStatus: {
+      type: String,
+      enum: ["single", "married", "prefer-not-to-say"],
+    },
+    address: { type: String },
+    dateOfBirth: { type: String },
+    bio: { type: String },
+    about: { type: String },
+    contact: { type: String },
+    resume: { type: String },
+    languages: [{ type: String }],
+  },
+
+  socials: {
+    githubLink: { type: String },
+    linkedInLink: { type: String },
+    twitterLink: { type: String },
+  },
+
+  skills: [
+    {
+      name: { type: String },
+      levelOfProficiency: {
+        type: String,
+        enum: ["beginner", "intermidiate", "advanced", "expert"],
+      },
+    },
+  ],
+
+  experience: [
+    {
+      companyName: { type: String },
+      role: { type: String },
+      responsibilty: { type: String },
+      skills: { type: String },
+      location: { type: String },
+      startDate: { type: String },
+      endDate: { type: String },
+    },
+  ],
+
+  education: [
+    {
+      schoolName: { type: String },
+      program: { type: String },
+      qualification: { type: String },
+      grade: { type: String },
+      location: { type: String },
+      startDate: { type: String },
+      endDate: { type: String },
+    },
+  ],
+
+  achievements: [
+    {
+      awards: { type: String },
+      desciption: { type: String },
+      image: { type: String },
+      date: { type: String },
+      nameOfInstitution: { type: String },
+    },
+  ],
+
+  projects: [
+    {
+      projectName: { type: String },
+      desciption: { type: String },
+      contributors: { type: String },
+      skills: { type: String },
+      links: { type: String },
+      nameOfInstitution: { type: String },
+    },
+  ],
+
+  volunteering: [
+    {
+      organization: { type: String },
+      desciption: { type: String },
+      skills: { type: String },
+      role: { type: String },
+      responsibilty: { type: String },
+      location: { type: String },
+      startDate: { type: String },
+      endDate: { type: String },
+      projectName: { type: String },
+    },
+  ],
+});
+
+export const UserModel = ("User", userSchema);
