843e8329e8793021caf2e5862c7935c9b1feb58d
owarewaa <akosuaowarewaa360@gmail.com>
2024-07-13 20:39:03 +0000

    volunteering crud

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { dbConnection } from "./config/db.js";
+import { volunteeringRouter } from "./routes/volunteering_routes.js";
 
 // creating express route
 const app = express();
 
-
+app.use(volunteeringRouter);
 
 // Call database
 dbConnection();
