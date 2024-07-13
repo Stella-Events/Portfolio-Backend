cf216901dea1d49752d1ed972cca5061b9d31420
owarewaa <akosuaowarewaa360@gmail.com>
2024-07-13 20:17:13 +0000

    done projects crud

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { dbConnection } from "./config/db.js";
+import { projectRouter } from "./routes/project_routes.js";
 
 // creating express route
 const app = express();
 
 
+app.use(projectRouter)
 
 // Call database
 dbConnection();
