0d8e161e888f6dc9fc2ac7ea1cffaff8d9c02307
encrentsil <nyamekye.nesther.esther599@gmail.com>
2024-07-23 10:19:41 +0000

    changes done

--- routes/user_profile_route.js
+++ routes/user_profile_route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllProfile, addUserProfile, patchProfile, getProfileById} from "../controllers/user_profile.js";
+import { addUserProfile, patchProfile, getUserProfile} from "../controllers/user_profile.js";
 import { remoteUpload } from "../middlewares/uploads.js";
 import { checkUserSession } from "../middlewares/auth.js";
 
@@ -9,14 +9,14 @@ const profileRouter = Router()
 
 //Creating routes
 
-profileRouter.get('/users/profiles/:id', checkUserSession, getProfileById)
+// profileRouter.get('/users/profiles/:id', checkUserSession, getProfileById)
 
 profileRouter.post('/users/profiles', checkUserSession, remoteUpload.fields([
     { name: "profilePicture", maxCount: 1 },
     { name: "resume", maxCount: 1 },
   ]), addUserProfile );
 
-profileRouter.get ('/users/profiles', checkUserSession, getAllProfile);
+profileRouter.get ('/users/profiles', checkUserSession, getUserProfile);
 
 profileRouter.patch ('/users/profiles/:id',checkUserSession, remoteUpload.fields([
   { name: "profilePicture", maxCount: 1 },
